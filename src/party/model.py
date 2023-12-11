3b09d822059a59a5f3d4bd40a22798df421b0069
Salmauva <135599070+Salmauva@users.noreply.github.com>
2023-12-11 14:34:01 +0100

    finding power and reistance

--- src/party/model.py
+++ src/party/model.py
@@ -53,7 +53,10 @@ class DiodeExperiment:
         i_list_plot = []
         u_list_error_plot = []
         i_list_error_plot = []
-
+        p_list_plot = []
+        r_list_plot = []
+        p_list_error_plot = []
+        r_list_error_plot = []
         # converting min,max value to ACD value
         voltage_min_acd = int((1023 * min) / 3.3)
         voltage_max_acd = int((1023 * max) / 3.3)
@@ -62,21 +65,31 @@ class DiodeExperiment:
         for x in range(voltage_min_acd, voltage_max_acd):
             u_list_per_point = []
             i_list_per_point = []
+            p_list_per_point = []
+            r_list_per_point = []
             self.device.set_output_value(x)
 
             # calculate per point the std and mean per n measurments
             for i in range(0, n):
                 u_chanel_1 = self.device.get_input_voltage(channel=1)
                 u_chanel_2 = self.device.get_input_voltage(channel=2)
-                u_value = u_chanel_1 - u_chanel_2
+                # u_value = u_chanel_1 - u_chanel_2
                 u_list_per_point.append(u_chanel_1*3)
                 ampere = u_chanel_2 / 4.7
                 i_list_per_point.append(ampere)
+                power = (u_chanel_1 * 3) * ampere
+                p_list_per_point.append(power)
+                resistance = (u_chanel_1 * 3) / ampere
+                r_list_per_point.append(resistance)
 
             # lists for the plot
             u_list_error_plot.append(np.std(u_list_per_point) / (n**0.5))
             i_list_error_plot.append(np.std(i_list_per_point) / (n**0.5))
+            p_list_error_plot.append(np.std(p_list_per_point) / (n**0.5))
+            r_list_error_plot.append(np.std(r_list_per_point) / (n**0.5))
             u_list_plot.append(np.mean(u_list_per_point))
             i_list_plot.append(np.mean(i_list_per_point))
+            p_list_plot.append(np.mean(p_list_per_point))
+            r_list_plot.append(np.mean(r_list_per_point))
             self.device.set_output_value(0)
-        return u_list_plot, i_list_plot, u_list_error_plot, i_list_error_plot
+        return u_list_plot, i_list_plot, u_list_error_plot, i_list_error_plot, p_list_plot, r_list_plot, p_list_error_plot,r_list_error_plot
